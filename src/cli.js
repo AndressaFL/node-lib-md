1dd1aeacf6a77e11aaf3bd5655dbb350bc040a22
AndressaFL <andressadefariaslazoti@gmail.com>
2023-01-31 10:43:08 -0600

    adding test files

--- src/cli.js
+++ src/cli.js
@@ -4,19 +4,34 @@ import fs from "fs";
 
 const path = process.argv;
 
+function printList(result, identifier = "") {
+  console.log(
+    chalk.yellow("Links List"),
+    chalk.black.bgGreen(identifier),
+    result
+  );
+}
+
 async function processText(arg) {
   const path = arg[2];
+  try {
+    fs.lstatSync(path);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.log("Cannot find the file or directory");
+      return;
+    }
+  }
 
   if (fs.lstatSync(path).isFile()) {
     const result = await getFile(arg[2]);
-    console.log(chalk.yellow("Links list"), result);
+    printList(result);
   } else if (fs.lstatSync(path).isDirectory()) {
     const files = await fs.promises.readdir(path);
     files.forEach(async (nameFiles) => {
       const list = await getFile(`${path}/${nameFiles}`);
-      console.log(list);
+      printList(list, nameFiles);
     });
-    console.log(files);
   }
 }
 
